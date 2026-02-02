03fe47aadf5fad889d986a5d2caaf35b0291cab7
Fabio Gouw <fabiogouw@gmail.com>
2026-02-02 15:39:31 -0300

    Bitnami images no longer have the version as tags :-(

--- spark-aws-messaging/src/test/java/com/fabiogouw/spark/awsmessaging/sqs/Spark3_5_1IntegrationTest.java
+++ spark-aws-messaging/src/test/java/com/fabiogouw/spark/awsmessaging/sqs/Spark3_5_1IntegrationTest.java
@@ -4,10 +4,10 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.IOException;
 
-@Testcontainers
+/*@Testcontainers
 public class Spark3_5_1IntegrationTest extends SparkIntegrationTest {
 
     public Spark3_5_1IntegrationTest() throws IOException {
         super("bitnami/spark:3.5.1");
     }
-}
+}*/
